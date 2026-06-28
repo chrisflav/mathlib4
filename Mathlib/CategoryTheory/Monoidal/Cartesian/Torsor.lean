e5719b8ad20a10e91169a3ce2b4ed8c032e0e2e9
Christian Merten <christian@merten.dev>
2026-06-28 11:35:26 +0200

    cleanup

--- Mathlib/CategoryTheory/Monoidal/Cartesian/Torsor.lean
+++ Mathlib/CategoryTheory/Monoidal/Cartesian/Torsor.lean
@@ -7,14 +7,12 @@ module
 
 public import Mathlib.CategoryTheory.Monoidal.Cartesian.Mod
 public import Mathlib.CategoryTheory.Sites.CoversTop.Basic
-public import Mathlib.CategoryTheory.Sites.Over
-public import Mathlib.CategoryTheory.Monoidal.Cartesian.Over
 
 /-!
 # Torsors in a cartesian monoidal category
 
 A module object `X` over a monoid object `M` is a torsor for the topology
-`J` if `M` acts simply transitively on `X` and `J`-locally `X` is trivial.
+`J` if `M` acts simply transitively on `X` and `locally `X` is trivial on some `J`-cover.
 -/
 
 @[expose] public section
@@ -24,7 +22,10 @@ universe v u
 namespace CategoryTheory.ModObj
 
 variable {C : Type u} [Category.{v} C] [CartesianMonoidalCategory C]
-variable {M : C} [MonObj M] {X Y : C} [ModObj M Y] [ModObj M X]
+  {M : C} [MonObj M] {X Y : C} [ModObj M Y] [ModObj M X]
+  {G : C} [GrpObj G] [ModObj G X]
+
+attribute [local instance] ModObj.regular
 
 open Limits
 
@@ -32,55 +33,42 @@ open CategoryTheory MonoidalCategory CartesianMonoidalCategory MonObj
 
 variable (J : GrothendieckTopology C)
 
-/-- A module object `X` is homogeneous over `M`, if the action is simply transitive, i.e.,
-the morphism `(m, x) ↦ (m • x, x)` is an isomorphism. -/
-abbrev IsHomogeneous (M : C) [MonObj M] (X : C) [ModObj M X] : Prop :=
-  IsIso (leftSMul M X)
-
 /-- A homogeneous module object is trivial over `U : C`, if there exists a morphism `U ⟶ X ⊗ U`. -/
+@[mk_iff]
 class IsTrivialOver (M : C) [MonObj M] (X : C) [ModObj M X] (U : C) [IsHomogeneous M X] where
-  nonempty_hom : Nonempty (U ⟶ X ⊗ U)
+  nonempty_hom : Nonempty (U ⟶ X)
 
-@[simp]
-lemma isTrivialOver_one_iff [IsHomogeneous M X] :
-    IsTrivialOver M X (𝟙_ C) ↔ Nonempty (𝟙_ C ⟶ X) :=
-  ⟨fun h ↦ ⟨h.nonempty_hom.some ≫ fst _ _⟩, fun h ↦ ⟨⟨h.some ≫ (ρ_ _).inv⟩⟩⟩
+lemma isTrivialOver_iff_isSplitEpi [IsHomogeneous M X] (U : C) :
+    IsTrivialOver M X U ↔ IsSplitEpi (snd X U) :=
+  ⟨fun h ↦ ⟨⟨⟨lift h.nonempty_hom.some (𝟙 _), by simp⟩⟩⟩, fun ⟨h⟩ ↦ ⟨⟨h.some.section_ ≫ fst _ _⟩⟩⟩
 
 variable (M X) in
 /-- A module object `X` over a monoid object `M` is a torsor for the Grothendieck topology `J`,
 if `M` acts simply transitively on `X` and there exists a `J`-covering that trivializes `X`. -/
 class IsTorsor (J : GrothendieckTopology C) (M : C) [MonObj M] (X : C) [ModObj M X] where
-  isHomogeneous : IsHomogeneous M X
+  isHomogeneous : IsHomogeneous M X := by infer_instance
   exists_coversTop : ∃ (I : Type max u v) (U : I → C),
     J.CoversTop U ∧ ∀ (i : I), IsTrivialOver M X (U i)
 
 namespace IsTorsor
 
-attribute [local instance] ModObj.regular
-
-instance : IsTorsor J M M where
-  isHomogeneous := by
-    simp
-    sorry
+instance : IsTorsor J G G where
   exists_coversTop := by
     refine ⟨PUnit, fun _ ↦ 𝟙_ _, ?_, ?_⟩
     · rw [J.coversTop_iff_of_isTerminal _ isTerminalTensorUnit,
         Sieve.pullback_ofObjects_eq_top (i := ⟨⟩) _ (𝟙 _)]
       simp
-    · simp only [isTrivialOver_one_iff, forall_const]
+    · simp only [isTrivialOver_iff, forall_const]
       exact ⟨η⟩
 
 end IsTorsor
 
-@[reassoc (attr := simp)]
-lemma inv_leftSMul_snd [IsIso (leftSMul M X)] :
-    inv (leftSMul M X) ≫ snd M X = snd _ _ := by
-  simp
-
 variable (M) in
+/-- A morphism `s : U ⟶ X` trivializes a homogeneous module object over `U`, i.e.,
+`X ⊗ U` is isomorphic to `M ⊗ U`.
+This corresponds to `X` being the trivial torsor when restricted to `Over U`. -/
 noncomputable
-def isoOfSection' {U : C} (s : U ⟶ X) [IsIso (leftSMul M X)] :
-    M ⊗ U ≅ X ⊗ U where
+def isoTensorOfHom {U : C} (s : U ⟶ X) [IsHomogeneous M X] : M ⊗ U ≅ X ⊗ U where
   hom := lift (𝟙 _ ⊗ₘ s) (snd _ _) ≫ γ[M, X] ▷ U
   inv := lift (𝟙 _ ⊗ₘ s) (snd _ _) ≫ inv (leftSMul M X) ▷ U ≫ fst _ _ ▷ U
   hom_inv_id := by
@@ -100,40 +88,50 @@ def isoOfSection' {U : C} (s : U ⟶ X) [IsIso (leftSMul M X)] :
     simp [h2]
 
 @[reassoc (attr := simp)]
-lemma isoOfSection'_hom_fst {U : C} (s : U ⟶ X) [IsIso (leftSMul M X)] :
-    (isoOfSection' M s).hom ≫ fst _ _ = _ ◁ s ≫ γ[M, X] := by
-  simp [isoOfSection']
+lemma isoTensorOfHom_hom_fst {U : C} (s : U ⟶ X) [IsHomogeneous M X] :
+    (isoTensorOfHom M s).hom ≫ fst _ _ = _ ◁ s ≫ γ[M, X] := by
+  simp [isoTensorOfHom]
 
 @[reassoc (attr := simp)]
-lemma isoOfSection'_hom_snd {U : C} (s : U ⟶ X) [IsIso (leftSMul M X)] :
-    (isoOfSection' M s).hom ≫ snd _ _ = snd _ _ := by
-  simp [isoOfSection']
+lemma isoTensorOfHom_hom_snd {U : C} (s : U ⟶ X) [IsHomogeneous M X] :
+    (isoTensorOfHom M s).hom ≫ snd _ _ = snd _ _ := by
+  simp [isoTensorOfHom]
 
 @[reassoc (attr := simp)]
-lemma isoOfSection'_inv_snd {U : C} (s : U ⟶ X) [IsIso (leftSMul M X)] :
-    (isoOfSection' M s).inv ≫ snd _ _ = snd _ _ := by
-  simp [isoOfSection']
+lemma isoTensorOfHom_inv_snd {U : C} (s : U ⟶ X) [IsHomogeneous M X] :
+    (isoTensorOfHom M s).inv ≫ snd _ _ = snd _ _ := by
+  simp [isoTensorOfHom]
 
-noncomputable
-def isoOfSection (s : 𝟙_ C ⟶ X) [IsIso (leftSMul M X)] : M ≅ X where
-  hom := lift (𝟙 _) (toUnit _ ≫ s) ≫ γ[M, X]
-  inv := lift (𝟙 _) (toUnit _ ≫ s) ≫ inv (leftSMul M X) ≫ fst _ _
-  hom_inv_id := by
-    have : lift (𝟙 M) (toUnit M ≫ s) ≫ γ[M, X] ≫ lift (𝟙 X) (toUnit X ≫ s) =
-        lift (𝟙 _) (toUnit M ≫ s) ≫ leftSMul M X := by
+/-- `isoTensorOfHom` is a module object homomorphism in `Over U`. To avoid passing
+to `Over U`, we state this as an equality of morphisms `C`. -/
+@[reassoc]
+lemma smul_isoTensorOfHom_hom {U : C} (s : U ⟶ X) [IsHomogeneous M X] :
+    μ ▷ U ≫ (isoTensorOfHom M s).hom =
+      (α_ _ _ _).hom ≫ M ◁ (isoTensorOfHom M s).hom ≫ (α_ _ _ _).inv ≫ γ[M, X] ▷ U := by
+  ext
+  · have : M ◁ M ◁ s ≫ M ◁ γ[M, X] =
+        (M ◁ lift (M ◁ s ≫ γ[M, X]) (snd M U) ≫ (α_ M X U).inv) ≫ fst (M ⊗ X) U := by
       ext <;> simp
-    simp [reassoc_of% this]
-  inv_hom_id := by
-    have h1 :
-      lift (𝟙 X) (toUnit X ≫ s) ≫ inv (leftSMul M X) ≫ fst M X ≫
-        lift (𝟙 M) (toUnit M ≫ s) =
-        lift (𝟙 X) (toUnit X ≫ s) ≫ inv (leftSMul M X) := by
-      ext <;> simp
-    have h2 : inv (leftSMul M X) ≫ γ[M, X] = fst X X := by simp
-    simp [reassoc_of% h1, h2]
+    simp [isoTensorOfHom, ← whisker_exchange_assoc, reassoc_of% this]
+  · simp
 
-lemma isSplitEpi_toUnit_iff [IsIso (leftSMul M X)] :
-    True :=
-  sorry
+variable (M) in
+/-- Any global section section `𝟙_ C ⟶ X`, induces an isomorphism `M ≅ X`. This
+is compatible with the module structure. -/
+noncomputable def isoOfHom (s : 𝟙_ C ⟶ X) [IsHomogeneous M X] : M ≅ X :=
+  (ρ_ _).symm ≪≫ isoTensorOfHom M s ≪≫ ρ_ _
+
+instance (s : 𝟙_ C ⟶ X) [IsHomogeneous M X] :
+    IsModHom M (isoOfHom M s).hom where
+  smul_hom := by
+    have : μ ≫ (ρ_ M).inv = (ρ_ _).inv ≫ μ ▷ 𝟙_ C := by simp
+    dsimp [isoOfHom]
+    simp only [Iso.trans_hom, Iso.symm_hom, reassoc_of% this, smul_isoTensorOfHom_hom_assoc]
+    simp
+
+lemma isTrivialOver_iff_nonempty_iso [IsHomogeneous M X] (U : C) :
+    IsTrivialOver M X U ↔ Nonempty (M ⊗ U ≅ X ⊗ U) :=
+  ⟨fun h ↦ ⟨isoTensorOfHom _ h.nonempty_hom.some⟩,
+    fun ⟨u⟩ ↦ ⟨⟨lift (toUnit _ ≫ η) (𝟙 _) ≫ u.hom ≫ fst _ _⟩⟩⟩
 
 end CategoryTheory.ModObj
